56a597c6343e60b18011a85b01a36da28a8c8261
KevinOfNeu <kevin@bless4.me>
2016-04-14 11:48:40 +0800

    init

---
+++ app/constants/hosts.js
@@ -0,0 +1,7 @@
+'use strict';
+export const BASE_URL = 'https://www.reddit.com/r/';
+export const POSTFIX = '.json';
+export const PICTURES = 'pics/';
+export const MOVIES = 'movies/';
+export const WORLD_NEWS = 'worldnews/';
+export const BOOKS = 'books/';
